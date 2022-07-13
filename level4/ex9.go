5a65948969be00e777fb610d51e42c10a8003252
Gabriel Almeida <gabriel.almeida@hurb.com>
2022-07-13 10:53:17 -0300

    feat(level4): ex9

---
+++ level4/ex9.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := map[string][]string{
+		"Almeida_Gabriel": {
+			"Plays guitar",
+			"Watch movies",
+			"Ride a bike",
+		},
+		"Cocorico_Julio": {
+			"Plays harmonica",
+			"Farming",
+			"Listen to country rock music",
+		},
+	}
+
+	x["OH_Yugi"] = []string{
+		"Play cards",
+		"Duel",
+	}
+
+	for index, value := range x {
+		fmt.Println(index)
+		for i, v := range value {
+			fmt.Println("\t", i, " - ", v)
+		}
+	}
+}
