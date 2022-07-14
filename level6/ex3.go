f1b9deb220b5023b987e90c726e304c124d79386
Gabriel Almeida <gabriel.almeida@hurb.com>
2022-07-14 17:47:35 -0300

    feat(level6): ex3

---
+++ level6/ex3.go
@@ -0,0 +1,11 @@
+/*
+- Utilize a declaração defer de maneira que demonstre que sua execução só ocorre ao final do contexto ao qual ela pertence.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("goes at the end")
+	fmt.Println("goes now")
+}
