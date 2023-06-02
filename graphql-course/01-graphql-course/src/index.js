4260e89dc10913ee488b9854ea0fea9df37b921e
almeigab <gabriel.almeida@hurb.com>
2023-06-02 14:46:04 -0300

    feat(graphql): add scalar types

--- graphql-course/01-graphql-course/src/index.js
+++ graphql-course/01-graphql-course/src/index.js
@@ -1,1 +1,28 @@
-console.log('Oi');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      id: ID!
+      name: String!
+      age: Int!
+      average: Float!
+      married: Boolean!
+      arrayString: [String!]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      id: () => '1asd987',
+      name: () => 'Gabriel Almeida',
+      age: () => 28,
+      average: () => 50.55,
+      married: () => false,
+      arrayString: () => ['A', 'B'],
+    },
+  },
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`Server listening on url ${url}`);
+});
